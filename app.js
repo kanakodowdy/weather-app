8ce104b588d9c03d7912576d7b98fa422dab0cb9
Kanako Dowdy <kanako.dowdy@gmail.com>
2021-08-27 21:31:55 -0500

    Set the app for imperial (F)

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "7e9cd1252a8ce0c8d0de0776fa7e9fac";
-  let unit = "metric";
+  let unit = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -103,7 +103,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "7e9cd1252a8ce0c8d0de0776fa7e9fac";
-  let unit = "metric";
+  let unit = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 
   axios.get(apiUrl).then(displayTemperature);
