060ee9dd72ac539a06eb7d50228444357cf670e4
Kanako Dowdy <kanako.dowdy@gmail.com>
2021-08-27 19:05:14 -0500

    added multiple day forecasts

--- app.js
+++ app.js
@@ -23,8 +23,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/03d@2x.png"
+                alt=""
+                width="60"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">36°</span>
+                <span class="weather-forecast-temperature-min">24°</span>
+              </div>
+            </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#city");
@@ -65,3 +92,4 @@ let form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
 
 search("St Louis");
+displayForecast();
